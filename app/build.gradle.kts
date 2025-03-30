02b025baddc3a7c88d0469f74d21a512b51a6b23
Alex <iampopovich@gmail.com>
2025-03-31 05:13:14 +0700

    removed old one name of project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.gpricescope"
+    namespace = "com.example.gogcrawler"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.gpricescope"
+        applicationId = "com.example.gogcrawler"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
